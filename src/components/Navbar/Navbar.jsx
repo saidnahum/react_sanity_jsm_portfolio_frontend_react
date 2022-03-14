79f31978672fbcf9af436b71ab387b42839b1af2
saidnahum <saidnahum@gmail.com>
2022-03-14 11:59:48 -0600

    Finish Course App

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                   >
                      <HiX onClick={() => setToggle(false)} />
                      <ul>
-                        {['home', 'about', 'work', 'skills', 'contanct'].map((item) => (
+                        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
                            <li key={item}>
                               <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                            </li>
